d2ce451f15e70218c7ddc2fef8253b53047c19f5
João <joao.morais@melius.software>
2024-11-17 16:26:51 -0300

    Alteração de conexão com o bd

--- src/conexao-bd.php
+++ src/conexao-bd.php
@@ -1,3 +1,8 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;dbname=autocenter', 'root', '486582Jp*');
+$host = 'fugfonv8odxxolj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'reog03h84z4u2wa4';
+$password = 'wbchnj87hi4i9is8';
+$database = 'osd15laayjaq8ov0';
+
+$pdo = new PDO('mysql:host='.$host.';dbname='.$database, $username, $password);
